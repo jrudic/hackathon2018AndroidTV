723062e5aa31c7221d4aed1e3a87ce46ef873a09
Jovan Rudic <m1rudic@gmail.com>
2018-12-05 18:25:56 +0100

    quiz...

---
+++ tvHub/src/main/java/rs/hydra/androidtv/quiz/score/ScoreActivity.java
@@ -0,0 +1,14 @@
+package rs.hydra.androidtv.quiz.score;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import rs.hydra.androidtv.R;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
