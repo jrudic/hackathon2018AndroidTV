5de560af20a03f40189d9c5409d978b476629704
jovanrudic <m1rudic@gmail.com>
2018-12-05 12:20:51 +0100

    dependencies

---
+++ tvHub/src/main/java/rs/hydra/androidtv/quiz/QuizActivity.java
@@ -0,0 +1,18 @@
+package rs.hydra.androidtv.quiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import rs.hydra.androidtv.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private QuizViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        viewModel = ViewModelProviders.of(this).get(InboxViewModel.class);
+    }
+}
